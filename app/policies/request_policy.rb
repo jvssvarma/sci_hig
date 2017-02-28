2efd8f59036dce64afb6693e192e81ddbbe4102f
jvssvarma <jampana.varma49@gmail.com>
2017-02-27 19:58:31 -0800

    refactor request permissions to users

--- app/policies/request_policy.rb
+++ app/policies/request_policy.rb
@@ -1,5 +1,20 @@
 class RequestPolicy < ApplicationPolicy
   def update?
-    record.user_id == user.id || user_types.include?(user.type)
+    return true if record_approved? && admin?
+    return true if user_or_admin && !record_approved?
+  end
+
+  private
+
+  def user_or_admin
+    record.user_id == user.id || admin?
+  end
+
+  def admin?
+    user_types.include?(user.type)
+  end
+
+  def record_approved?
+    record.approved?
   end
 end
--- spec/features/workflow_spec.rb
+++ spec/features/workflow_spec.rb
@@ -6,4 +6,30 @@ describe 'navigate approval workflow' do
     login_as(@admin_user, :scope => :user)
   end
 
+  describe 'edit' do
+    before do
+      @request = FactoryGirl.create(:request)
+      visit edit_request_path(@request)
+    end
+
+    it "restricts non admin users from editing admin requests" do
+      @request.approved!
+      logout(:user)
+      user = FactoryGirl.create(:user)
+      login_as(user, :scope => :user)
+      visit edit_request_path(@request)
+      expect(page).to_not have_content('Approved')
+    end
+
+    it "restrictes non admin users from editing their requests after approval" do
+      @request.approved!
+      logout(:user)
+      user = FactoryGirl.create(:user)
+      login_as(user, :scope => :user)
+      @request.update(user_id: user.id, status: "approved")
+      visit edit_request_path(@request)
+      expect(current_path).to eq(root_path)
+    end
+  end
+
 end
