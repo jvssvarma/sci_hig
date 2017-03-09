740b6ec2900b347835ae7df3f7ab50432452dc0a
jvssvarma <jampana.varma49@gmail.com>
2017-03-09 09:51:50 -0800

    Add validations for audit log

--- spec/factories/audit_logs.rb
+++ spec/factories/audit_logs.rb
@@ -2,7 +2,7 @@ FactoryGirl.define do
   factory :audit_log do
     user
     status 0
-    start_date (Date.today - 1.days)
+    start_date (Date.yesterday)
     end_date "2017-03-06"
   end
 end
--- spec/models/audit_log_spec.rb
+++ spec/models/audit_log_spec.rb
@@ -1,5 +1,33 @@
 require 'rails_helper'
 
 RSpec.describe AuditLog, type: :model do
-  
+  before do
+    @auditlog = FactoryGirl.create(:audit_log)
+  end
+
+  describe "creating AuditLog" do
+    it "should be able to create auditlog properly" do
+      expect(@auditlog).to be_valid
+    end
+
+    it "must have a user associated" do
+      @auditlog.user_id = nil
+      expect(@auditlog).to_not be_valid
+    end
+
+    it "must have a status" do
+      @auditlog.status = nil
+      expect(@auditlog).to_not be_valid
+    end
+
+    it "must have a valid start_date" do
+      @auditlog.start_date = nil
+      expect(@auditlog).to_not be_valid
+    end
+
+    it "must have start_date as yesterday by default" do
+      audit_log1 = AuditLog.create(user_id: User.last.id)
+      expect(audit_log1.start_date).to eq(Date.yesterday)
+    end
+  end
 end
