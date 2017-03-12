3ec8efe08bc1f93298652d9538939878a21128a9
jvssvarma <jampana.varma49@gmail.com>
2017-03-12 16:05:47 -0700

    Add production mail config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -19,6 +19,7 @@ Rails.application.configure do
     config.logger = ActiveSupport::TaggedLogging.new(logger)
   end
 
+  config.action_mailer.default_url_options = { :host => 'immense-taiga-45522.herokuapp.com/'}
   config.active_record.dump_schema_after_migration = false
   config.serve_static_assets = true
   config.assets.digest = true
