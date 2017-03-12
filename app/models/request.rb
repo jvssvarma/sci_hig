f85e19b03d98d716be687ea9c75ba48b906969a2
jvssvarma <jampana.varma49@gmail.com>
2017-03-12 12:56:36 -0700

    Add callback for updating audit log if new request is created

--- app/models/request.rb
+++ app/models/request.rb
@@ -4,4 +4,12 @@ class Request < ApplicationRecord
   validates :request_hours, numericality: { greater_than: 0.0 }
   enum status: { submitted: 0, approved: 1, rejected: 2 }
   scope :requests_added_by, -> (user) { where(user_id: user.id) }
+  after_save :update_audit_log
+
+  private
+
+  def update_audit_log
+    audit_log = AuditLog.where(user_id: self.user.id, start_date: (1.week.ago.beginning_of_week)).last
+    audit_log.confirmed!
+  end
 end
